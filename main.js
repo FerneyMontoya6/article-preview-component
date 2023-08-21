ab2a15c887ec3b0689c5a3f3ddcc6dcb45b8d61c
Ferney <ferney.montoyag@gmail.com>
2023-08-21 11:17:28 -0500

    First mobile version finished

---
+++ main.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('button');
+const bgShare = document.querySelector('.background-share');
+
+btn.addEventListener('click', () => {
+    //bgShare.classList.toggle('hidden');
+    bgShare.classList.toggle('background-share__active');
+    btn.classList.toggle('card--main-description--profile__share__active');
+});
