6fe71671fa8d712624a89d55ec017c6d3d3bde07
Ferney <ferney.montoyag@gmail.com>
2023-08-21 14:38:12 -0500

    First version whole app completed

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ const bgShare = document.querySelector('.background-share');
 btn.addEventListener('click', () => {
     //bgShare.classList.toggle('hidden');
     bgShare.classList.toggle('background-share__active');
+
     btn.classList.toggle('card--main-description--profile__share__active');
 });
